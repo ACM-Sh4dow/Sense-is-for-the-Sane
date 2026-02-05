aae5e67a84ba77cf2c766f54ac1c5a99e661adac
ww3564 <ww3564@my.acm.ac.uk>
2026-02-05 16:57:31 +0000

    Made a lot of changes
    Saving kind of works, new game, load(maybe), settings and quit works, pause feature added for future use.

--- Assets/Scripts/SavingAndLoading.cs
+++ Assets/Scripts/SavingAndLoading.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class Saving_And_Loading : MonoBehaviour
 {
-    [System.Serializable]
+[System.Serializable]
     public class SaveData
     {
         public Vector3 playerPos;
@@ -11,7 +11,8 @@ public class Saving_And_Loading : MonoBehaviour
     public static void SaveCurrentData()
     {
         SaveData newSaveData = new SaveData();
-        newSaveData.playerPos = ProtagonistController.i.transform.position;
+        //This is currently not working I think it's because there isn't an object to record the player save data I'm most likely wrong about this
+        //newSaveData.playerPos = ProtagonistController.i.transform.position;
 
         string convertedData = JsonUtility.ToJson(newSaveData);
         System.IO.File.WriteAllText(Application.persistentDataPath + "/SaveData.json", convertedData);
@@ -29,6 +30,7 @@ public class Saving_And_Loading : MonoBehaviour
 
     public static void DeleteAllData()
     {
+        //Deletes the Json file to find it and manually delete the file you need to go to Users -> your account name -> AppData -> LowcalLow -> business name
         System.IO.File.Delete(Application.persistentDataPath + "/SaveData.json");
     }
 
