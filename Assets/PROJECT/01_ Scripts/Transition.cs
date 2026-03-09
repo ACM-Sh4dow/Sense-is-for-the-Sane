e84d04d7b503ea6bbecbe50a2c5c9ca2b32504f4
ACM-Sh4dow <km3391@my.acm.ac.uk>
2026-03-09 12:49:30 +0000

    FEATURE: Added Retrograde

--- Assets/PROJECT/01_ Scripts/Transition.cs
+++ Assets/PROJECT/01_ Scripts/Transition.cs
@@ -1,11 +1,14 @@
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class Transition : MonoBehaviour, InteractionPoint
 {
     public Vector3 transitionDirection;
-    
+    public FullScreenPassRendererFeature retrograde;
+
     public void Interact()
     {
         ProtagonistController.Instance.transform.position += transitionDirection;
+        retrograde.SetActive(!retrograde.isActive);
     }
 }
