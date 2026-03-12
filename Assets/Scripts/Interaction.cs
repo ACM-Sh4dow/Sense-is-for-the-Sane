7cfdd74618661c85df2e2b8e33325be43ad82453
ww3564 <ww3564@my.acm.ac.uk>
2026-03-12 16:21:44 +0000

    rotation xyzw original position now saves

--- Assets/Scripts/Interaction.cs
+++ Assets/Scripts/Interaction.cs
@@ -1,3 +1,4 @@
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -8,18 +9,16 @@ interface IInteracable
 
 public class Interaction : MonoBehaviour
 {
-    public Transform InteractionSource;
     public float InteractionRange;
 
     private GameObject ActiveObject;
     private Transform OriginalPosition;
-
     private ProtagonistController controller;
 
     // Update is called once per frame
-
     private void Start()
     {
+        // Getting reference from ProtagonistController script
         controller = GetComponent<ProtagonistController>();
     }
     void Update()
@@ -29,14 +28,26 @@ public class Interaction : MonoBehaviour
             Ray r = new Ray(this.gameObject.transform.position, this.gameObject.transform.forward);
             if (Physics.Raycast(r, out RaycastHit hitInfo, InteractionRange))
             {
+                // Unlocks the cursor
                 Cursor.lockState = CursorLockMode.None;
 
-
+                // Lock player movement
                 controller.islocked = true;
-                //lock player movement
 
                 ActiveObject = hitInfo.collider.gameObject;
                 OriginalPosition = ActiveObject.transform;
+
+                // Saving the xyz of the roation
+                float x = OriginalPosition.transform.rotation.x;
+                float y = OriginalPosition.transform.rotation.y;
+                float z = OriginalPosition.transform.rotation.z;
+                float w = OriginalPosition.transform.rotation.w;
+
+
+                PlayerPrefs.SetFloat("RotationX", x);
+                PlayerPrefs.SetFloat("RotationY", y);
+                PlayerPrefs.SetFloat("RotationZ", z);
+                PlayerPrefs.SetFloat("RotationW", w);
             }
         }
         if (Input.GetKeyDown(KeyCode.Escape))
@@ -44,12 +55,22 @@ public class Interaction : MonoBehaviour
             Ray r = new Ray(this.gameObject.transform.position, this.gameObject.transform.forward);
             if (Physics.Raycast(r, out RaycastHit hitInfo, InteractionRange))
             {
+                // Locks the cursor
                 Cursor.lockState = CursorLockMode.Locked;
-                // ActiveObject.transform.rotation = OriginalPosition.rotation.eulerAngles(;
 
+                // Player can now move
                 controller.islocked = false;
-                //reset object to original rotation
-                // ActiveObject.transform set it to OriginalPosition
+
+                // Loading the xyz of the rotation
+                float x = PlayerPrefs.GetFloat("RotationX");
+                float y = PlayerPrefs.GetFloat("RotationY");
+                float z = PlayerPrefs.GetFloat("RotationZ");
+                float w = PlayerPrefs.GetFloat("RotationW");
+
+                quaternion savedRotation = new quaternion(x, y, z, w);
+
+                // Reset object to original rotation
+                OriginalPosition.transform.rotation = savedRotation;
             }
         }
     }
