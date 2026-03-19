69d4abf5e836b76ce5b0e0450e0a05c9149e938b
ACM-Sh4dow <km3391@my.acm.ac.uk>
2026-03-19 18:48:04 +0000

    FH Audio Fix

--- Assets/PROJECT/01_ Scripts/Transition.cs
+++ Assets/PROJECT/01_ Scripts/Transition.cs
@@ -6,9 +6,13 @@ public class Transition : MonoBehaviour, InteractionPoint
     public Vector3 transitionDirection;
     public GameObject retrograde;
 
+    public string audioStateName;
+
     public void Interact()
     {
         PlayerBehaviour.Instance.transform.position += transitionDirection;
         retrograde.SetActive(!retrograde.activeSelf);
+
+        AkUnitySoundEngine.SetState("FuneralHomeVersion", audioStateName);
     }
 }
