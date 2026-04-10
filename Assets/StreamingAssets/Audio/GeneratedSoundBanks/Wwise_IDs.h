18a3ec56eb6e20feae0d6ac29b62bee84879478f
LewisAxfordPapageorgiou <lewisaxfordpapageorgiou@gmail.com>
2026-04-10 21:14:04 +0100

    Rearranging Soundbanks + Extra Void Ambience

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -18,9 +18,9 @@ namespace AK
         static const AkUniqueID APT_FIREPLACE = 2202553018U;
         static const AkUniqueID APT_FRIDGE = 2280025682U;
         static const AkUniqueID APT_SINK = 751812660U;
+        static const AkUniqueID DOOR_CREAKING = 2843028372U;
         static const AkUniqueID FNRL_AMB_PAUSE = 2963388045U;
         static const AkUniqueID FNRL_AMB_THUNDERSTORM = 1612838152U;
-        static const AkUniqueID FNRL_DOOR_CREAKING = 3003594117U;
         static const AkUniqueID FNRL_FLOWER_VASE = 3533695665U;
         static const AkUniqueID FNRL_MIRROR_BREATHING = 1736817776U;
         static const AkUniqueID FNRL_MIRROR_COMPLETE = 305829503U;
