457fb9b707a43f205de30117d8a12c0c83e3b0a4
pri (homepc) <pm4090@my.acm.ac.uk>
2026-04-16 23:40:46 +0100

    Apartment transition changes

--- Assets/PROJECT/01_ Scripts/CloseOffApartment.cs
+++ Assets/PROJECT/01_ Scripts/CloseOffApartment.cs
@@ -1,31 +1,18 @@
 using System;
 using UnityEngine;
+using UnityEngine.ProBuilder.Shapes;
 
 public class CloseOffApartment : MonoBehaviour
 {
-    private enum colliderType
-    {
-        transitionTrigger,
-        invisibleWall
-    }
-    [Tooltip("Invisible Wall is blocking the player from returning to the apartment. Transition Trigger is unloading previous scenes.")]
-    [SerializeField] private colliderType colliderIs;
+    [SerializeField] private GameObject funeralHomeDoor;
     private void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            Debug.Log("Player exited collider.");
-            switch (colliderIs)
-            {
-                case colliderType.invisibleWall:
-                    transform.GetComponent<BoxCollider>().isTrigger = false; //possible soft lock if the player walks backwards into AP
-                    //set music state
-                    break;
-                case colliderType.transitionTrigger:
-                    transform.GetComponent<SceneTransition>().TriggerTransition();
-                    //close door
-                    break;
-            }
+
+            transform.GetComponent<SceneTransition>().TriggerTransition();
+            funeralHomeDoor.SetActive(true);
+            //set music state
         }
     }
 }
